eae46b2070336e67f5cf55fb2bf2af563c226e74
Jean Raphael Bordet <jr.bordet@touchwa.re>
2019-04-11 08:22:56 +0200

    Initial commit

---
+++ JRSkeleton/JRSkeleton/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JRSkeleton
+//
+//  Created by Jean Raphael Bordet on 10/04/2019.
+//  Copyright © 2019 Touchware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
